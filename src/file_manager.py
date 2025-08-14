1cbdbb80df6378970892fb4b04fbf14d930f69fb
luana <llmaf@cesar.school>
2025-08-14 16:57:13 -0300

    função salvar_livros

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,3 +1,4 @@
 def salvar_livros(livros):
     with open("data/biblioteca.txt", "a") as f:
-        f.writelines(livros)
+        for i in range(len(livros)):
+            f.write(f'{livros[i].get('nome')}, {livros[i].get('autor')}, {livros[i].get('categoria')}, {livros[i].get('valor')}\n')
