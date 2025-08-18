8937d21bb182f79dc09f5034028c05c84c9c86d4
luana <llmaf@cesar.school>
2025-08-18 10:57:47 -0300

    Adiciona codificação UTF-8 e simplifica a função salvar_livros para melhorar a escrita dos dados

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,4 +1,5 @@
 def salvar_livros(livros):
-    with open("data/biblioteca.txt", "a") as f:
-        for i in range(len(livros)):
-            f.write(f'{livros[i].get('nome')}, {livros[i].get('autor')}, {livros[i].get('categoria')}, {livros[i].get('valor')}\n')
+    with open("data/biblioteca.txt", "a", encoding="utf-8") as arquivo:
+        for livro in livros:
+            arquivo.write(f"{livro['nome']}, {livro['autor']}, {livro['categoria']}, {livro['valor']}\n")
+        
