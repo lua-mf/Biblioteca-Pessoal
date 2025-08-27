61e76d9b7ef6fdee4430e970c842d2ad191335bc
luana <llmaf@cesar.school>
2025-08-27 17:18:41 -0300

    Renomeia a função salvar_livros para salvar_biblioteca e ajusta a função main para utilizar a biblioteca carregada

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,5 +1,31 @@
-def salvar_livros(livros):
+import os
+def salvar_biblioteca(livros):
     with open("data/biblioteca.txt", "a", encoding="utf-8") as arquivo:
         for livro in livros:
             arquivo.write(f"{livro['nome']}, {livro['autor']}, {livro['categoria']}, {livro['valor']}\n")
         
+def carregar_biblioteca():
+    livros = []
+    try:
+        if os.path.getsize("data/biblioteca.txt") == 0:
+            print(f"O arquivo biblioteca.txt está vazio.")
+            return []
+        with open("data/biblioteca.txt", "r", encoding="utf-8") as arquivo:
+            for livro in livros:
+                livro = livro.split(',')
+
+                dic = {
+                    "nome": livro[0].strip(),
+                    "autor": livro[1].strip(),
+                    "categoria": livro[2].strip(),
+                    "valor": livro[3].strip
+                }
+
+                livros.append(dic)
+        print(f"Biblioteca carregada com sucesso!")
+    except FileNotFoundError:
+        print(f"Arquivo biblioteca.txt não encontrado.")
+    except Exception as e:
+        print(f"Erro ao carregar o arquivo: {e}")
+        return livros
+            
--- src/main.py
+++ src/main.py
@@ -6,29 +6,29 @@ os.system("cls")
 
 def main():
     # lista de dicionarios, cada dicionario é um livro
-    livros = []
+    biblioteca = file_manager.carregar_biblioteca()
 
     while True:
         # CRUD
         print('\n--------- Menu ---------\n 1 - Adicionar\n 2 - Visualizar\n 3 - Atualizar\n 4 - Excluir\n 5 - Sair')
         resposta_1 = int(input())
         if resposta_1 == 1:
-            resultado = utils.adicionar(livros)
+            resultado = utils.adicionar(biblioteca)
             print(resultado)
         elif resposta_1 == 2:
             print(
                 'a) Todos os livros\nb) Filtrar por categoria\nc) Extrato por categoria\nd) Controle de Gastos')
             resposta_2 = input()
             if resposta_2 == 'a':
-                utils.visualizar_todos(livros)
+                utils.visualizar_todos(biblioteca)
             elif resposta_2 == 'b':
-                utils.filtrar_por_categoria(livros)
+                utils.filtrar_por_categoria(biblioteca)
             elif resposta_2 == 'c':
-                utils.extrato_por_categoria(livros)
+                utils.extrato_por_categoria(biblioteca)
             elif resposta_2 == 'd':
-                utils.controle_de_gastos(livros)
+                utils.controle_de_gastos(biblioteca)
         elif resposta_1 == 5:
-            file_manager.salvar_livros(livros)
+            file_manager.salvar_livros(biblioteca)
             break
 
 
